ac2450d169b099e76cd49555c0236a98efe6e98f
Rudy <rudydimacali@gmail.com>
2019-02-14 14:41:03 -0800

    Complete fib.js.

---
+++ section8/fib.js
@@ -0,0 +1,26 @@
+const assert = require('assert');
+
+/* Write a function that takes a number n and returns the nth number
+in the Fibonacci sequence.
+
+ I: Number
+ O: N number in the Fibonacci sequence.
+ C: N/A
+ E: N/A
+ */
+
+const fib = (n) => {
+  if (n === 1 || n === 2) return 1;
+  const fibs = [1, 1];
+  const addFib = () => {
+    fibs.push(fibs[fibs.length - 1] + fibs[fibs.length - 2]);
+    if (fibs.length < n) { addFib(); }
+  };
+  addFib();
+  return fibs[n - 1];
+};
+
+assert.equal(fib(4), 3);
+assert.equal(fib(10), 55);
+assert.equal(fib(28), 317811);
+assert.equal(fib(35), 9227465);
