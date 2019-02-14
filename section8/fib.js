1584cb2383b787254f8b1777176b08194e8f3896
Rudy <rudydimacali@gmail.com>
2019-02-14 14:42:25 -0800

    Add pure recursion solution for fib.

--- section8/fib.js
+++ section8/fib.js
@@ -24,3 +24,13 @@ assert.equal(fib(4), 3);
 assert.equal(fib(10), 55);
 assert.equal(fib(28), 317811);
 assert.equal(fib(35), 9227465);
+
+const fibPure = (n) => {
+  if (n <= 2) return 1;
+  return fib(n - 1) + fib(n - 2);
+}
+
+assert.equal(fibPure(4), 3);
+assert.equal(fibPure(10), 55);
+assert.equal(fibPure(28), 317811);
+assert.equal(fibPure(35), 9227465);
