bba78a6787f49538a0127e42986e70dcb2f7abd4
Rudy <rudydimacali@gmail.com>
2019-02-22 09:15:43 -0800

    Complete BST.insert.

--- section22/binarySearchTree.js
+++ section22/binarySearchTree.js
@@ -10,4 +10,33 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  insert(val) {
+    const newNode = new Node(val);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+      // eslint-disable-next-line no-else-return
+    } else {
+      let node = this.root;
+      // eslint-disable-next-line no-constant-condition
+      while (true) {
+        if (val === node.val) return undefined;
+        if (val < node.val && node.left === null) {
+          node.left = newNode;
+          return this;
+        }
+        if (val < node.val) {
+          node = node.left;
+        }
+        if (val > node.val && node.right === null) {
+          node.right = newNode;
+          return this;
+        }
+        if (val > node.val) {
+          node = node.right;
+        }
+      };
+    }
+  }
 }
