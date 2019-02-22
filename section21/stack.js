cb682096ff177b94d3a8a87912f4df3e967fa18e
Rudy <rudydimacali@gmail.com>
2019-02-22 08:40:16 -0800

    Complete Stack.js.

--- section21/stack.js
+++ section21/stack.js
@@ -35,3 +35,19 @@ class Stack {
     return removedNode;
   }
 }
+
+const findMaxDiff = (nums) => {
+  let highestItem = nums[0];
+  let highestDiff = 0;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i - 1] > highestItem) {
+      highestItem = nums[i - 1];
+    }
+    if (nums[i] - highestItem > highestDiff) {
+      highestDiff = nums[i] - highestItem;
+    }
+  }
+  return highestDiff;
+};
+
+findMaxDiff(1)
