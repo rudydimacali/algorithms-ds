804d9185e9051678ecad7042ae0f339068dbd52c
Rudy <rudydimacali@gmail.com>
2019-02-27 11:14:17 -0800

    Complete HashTable.

--- section25/hashTable.js
+++ section25/hashTable.js
@@ -1,3 +1,7 @@
+/*
+Hashing Functions
+*/
+
 const simpleHash = (keyStr, length) => {
   let total = 0;
   for (let i = 0; i < keyStr.length; i += 1) {
@@ -15,6 +19,16 @@ const betterHash = (keyStr, length) => {
   return total;
 };
 
+/*
+
+Hash Tables are used to store key:value pairs within buckets.
+Time Complexity:
+
+Insertion: O(1)
+Deletion: O(1)
+Access: O(1)
+
+*/
 
 class HashTable {
   constructor(size = 53) {
@@ -37,7 +51,11 @@ class HashTable {
     if (this.keyMap[index] === undefined) {
       this.keyMap[index] = [];
     }
+    this.keyMap[index].forEach((bucket) => {
+      if (bucket[0] === key) return undefined;
+    });
     this.keyMap[index].push([key, value]);
+    return this.keyMap;
   }
 
   get(key) {
@@ -50,5 +68,25 @@ class HashTable {
       }
     }
     return undefined;
+  }
+
+  keys() {
+    const keys = [];
+    this.keyMap.forEach((bucket) => {
+      bucket.forEach((pair) => {
+        if (!keys.includes(pair[0])) keys.push(pair[0]);
+      });
+    });
+    return keys;
+  }
+
+  values() {
+    const values = [];
+    this.keyMap.forEach((bucket) => {
+      bucket.forEach((pair) => {
+        if (!values.includes(pair[1])) values.push(pair[1]);
+      });
+    });
+    return values;
   }
 }
