7b69fe932c45a1ce5821317edf3f88ce8653bd95
Rudy <rudydimacali@gmail.com>
2019-02-27 09:47:00 -0800

    Initialize HashTable class.

--- section25/hashTable.js
+++ section25/hashTable.js
@@ -14,3 +14,21 @@ const betterHash = (keyStr, length) => {
   }
   return total;
 };
+
+
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
+  }
+
+  _hash(key) {
+    let total = 0;
+    const PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i += 1) {
+      const char = key[i];
+      const value = char.charCodeAt(0) - 96;
+      total = (total * PRIME + value) % this.keyMap.length;
+    }
+    return total;
+  }
+}
