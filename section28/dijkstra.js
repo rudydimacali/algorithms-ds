771d0a5a079c6621f1ee097a5434728b9a1c1762
Rudy <rudydimacali@gmail.com>
2019-03-01 09:25:27 -0800

    Fix linting errors.

--- section28/dijkstra.js
+++ section28/dijkstra.js
@@ -39,8 +39,8 @@ class PriorityQueue {
     const element = this.values[0];
     let leftChild = this.values[(index * 2) + 1];
     let rightChild = this.values[(index * 2) + 2];
-    while ((leftChild !== undefined && element.priority > leftChild.priority) ||
-      (rightChild !== undefined && element.priority > rightChild.priority)) {
+    while ((leftChild !== undefined && element.priority > leftChild.priority)
+      || (rightChild !== undefined && element.priority > rightChild.priority)) {
       if (rightChild === undefined || rightChild > leftChild) {
         this.swap(index, (index * 2) + 2);
         index = (index * 2) + 2;
@@ -116,22 +116,22 @@ class WeightedGraph {
   }
 }
 
-var graph = new WeightedGraph()
-graph.addVertex("A");
-graph.addVertex("B");
-graph.addVertex("C");
-graph.addVertex("D");
-graph.addVertex("E");
-graph.addVertex("F");
+const graph = new WeightedGraph();
+graph.addVertex('A');
+graph.addVertex('B');
+graph.addVertex('C');
+graph.addVertex('D');
+graph.addVertex('E');
+graph.addVertex('F');
 
-graph.addEdge("A", "B", 4);
-graph.addEdge("A", "C", 2);
-graph.addEdge("B", "E", 3);
-graph.addEdge("C", "D", 2);
-graph.addEdge("C", "F", 4);
-graph.addEdge("D", "E", 3);
-graph.addEdge("D", "F", 1);
-graph.addEdge("E", "F", 1);
+graph.addEdge('A', 'B', 4);
+graph.addEdge('A', 'C', 2);
+graph.addEdge('B', 'E', 3);
+graph.addEdge('C', 'D', 2);
+graph.addEdge('C', 'F', 4);
+graph.addEdge('D', 'E', 3);
+graph.addEdge('D', 'F', 1);
+graph.addEdge('E', 'F', 1);
 
 
 console.log(graph.shortestPath('A', 'F'));
