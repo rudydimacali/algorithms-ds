d32ce525ad35cc6f3916b8902f399e3217c11af0
Rudy <rudydimacali@gmail.com>
2019-03-01 10:22:42 -0800

    Complete fibonacciTabulated.

--- section29/fibonacci.js
+++ section29/fibonacci.js
@@ -14,7 +14,7 @@ const fibonacciRecursive = (n) => {
 /*
 
 Dynamic Programming Time Complexity:
-O(n)
+O(n) -> CAN EXCEED MAXIMUM CALL STACK
 
 */
 const fibonacciDynamic = (n, memo = []) => {
@@ -23,4 +23,20 @@ const fibonacciDynamic = (n, memo = []) => {
   const num = fibonacciDynamic(n - 1, memo) + fibonacciDynamic(n - 2, memo);
   memo[n] = num;
   return num;
+};
+
+/*
+
+Tabulated Programming Time Complexity:
+O(n) -> WON'T EXCEED MAXIMUM CALL STACK
+
+*/
+
+const fibonacciTabulated = (n) => {
+  if (n <= 2) return 1;
+  const fibNums = [1, 1];
+  for (let i = 2; i < n; i += 1) {
+    fibNums[i] = fibNums[i - 1] + fibNums[i - 2];
+  }
+  return fibNums[n - 1];
 };
